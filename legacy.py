888e77a1d25bb61f6da85899fa98806b12fdb905
Jon Loveday <j.loveday@sussex.ac.uk>
2024-05-23 10:38:05 +0100

    Update legacy.py
    Remove i-band selection from select function, as i-band coverage is incomplete.

--- legacy.py
+++ legacy.py
@@ -45,8 +45,9 @@ def select(maglim=22,
     """Select subset of Legacy data."""
 
     # Extract Legacy sources with mag_z < maglim from sweep files
+    # Ignore i-band data as coverage is incomplete
     sweeps = np.array(glob.glob(path + 'sweep*.fits'))
-    ra, dec, mag_g, mag_r, mag_i, mag_z, ltype = np.array(()), np.array(()), np.array(()), np.array(()), np.array(()), np.array(()), np.array(())
+    ra, dec, mag_g, mag_r, mag_z, ltype = np.array(()), np.array(()), np.array(()), np.array(()), np.array(()), np.array(())
     for sweep in sweeps:
         t = Table.read(sweep)
         # Mask cuts
@@ -60,27 +61,23 @@ def select(maglim=22,
         # Mag and colour cuts
         flux_g = t['FLUX_G']/t['MW_TRANSMISSION_G']
         flux_r = t['FLUX_R']/t['MW_TRANSMISSION_R']
-        flux_i = t['FLUX_I']/t['MW_TRANSMISSION_I']
         flux_z = t['FLUX_Z']/t['MW_TRANSMISSION_Z']
         good = ((flux_g > 0) * (flux_g < 1e6) * (flux_r > 0) * (flux_r < 1e6) *
-                (flux_i > 0) * (flux_i < 1e6) * (flux_z > 0) * (flux_z < 1e6))
+                (flux_z > 0) * (flux_z < 1e6))
         t = t[good]
         magg = 22.5 - 2.5*np.log10(flux_g[good])
         magr = 22.5 - 2.5*np.log10(flux_r[good])
-        magi = 22.5 - 2.5*np.log10(flux_i[good])
         magz = 22.5 - 2.5*np.log10(flux_z[good])
         sel = (magz < maglim) * (magg - magr > -1) * (magg - magr < 4) * (magr - magz > -1) * (magr - magz < 4)
         t = t[sel]        
         magg = magg[sel]
         magr = magr[sel]
-        magi = magi[sel]
         magz = magz[sel]
         
         ra = np.hstack((ra, t['RA']))
         dec = np.hstack((dec, t['DEC']))
         mag_g = np.hstack((mag_g, magg))
         mag_r = np.hstack((mag_r, magr))
-        mag_i = np.hstack((mag_i, magi))
         mag_z = np.hstack((mag_z, magz))
         ltype = np.hstack((ltype, t['TYPE']))
         print(sweep, len(mag_z))
@@ -89,7 +86,7 @@ def select(maglim=22,
     glat = c.galactic.b
     
     # Write out the selected galaxies
-    t = Table((ra, dec, mag_g, mag_r, mag_i, mag_z, ltype), names=('RA', 'DEC', 'G_MAG', 'R_MAG', 'I_MAG', 'Z_MAG', 'LTYPE'))
+    t = Table((ra, dec, mag_g, mag_r, mag_z, ltype), names=('RA', 'DEC', 'G_MAG', 'R_MAG', 'Z_MAG', 'LTYPE'))
     t[glat > 0].write(galout.format('ngc'), overwrite=True)
     t[glat < 0].write(galout.format('sgc'), overwrite=True)
     
@@ -100,7 +97,7 @@ def select(maglim=22,
     for iran in range(20):
         t = Table.read(ranfile.format(iran))
         ntot = len(t)
-        sel = t['MASKBITS'] == 0
+        sel = (t['MASKBITS'] == 0) * (NOBS_G > 0) * (NOBS_R > 0) * (NOBS_Z > 0)
         t = t[sel]
         nsel = len(t)
         t = Table((t['RA'], t['DEC']), names=('RA', 'DEC'))
