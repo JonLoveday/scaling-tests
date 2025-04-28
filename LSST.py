a824530152ef675f2d26e86f302d845dc64a2311
Jon Loveday <j.loveday@sussex.ac.uk>
2025-04-28 15:59:27 +0100

    Add default filenames

--- LSST.py
+++ LSST.py
@@ -4,7 +4,7 @@ from astropy.table import Table, join
 import matplotlib.pyplot as plt
 import numpy as np
 
-def check_mask(datafile, maskfile):
+def check_mask(datafile='edfs.fits', maskfile='edfs.ply'):
     '''Plot results of LSST query and GAIA bright star mask.'''
     t = Table.read(datafile)
     mask = np.loadtxt(maskfile, skiprows=2)
