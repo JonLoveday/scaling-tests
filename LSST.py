dbe6dd45d14d410d3be14e33e34f9b12fe94a2ab
Jon Loveday <j.loveday@sussex.ac.uk>
2025-04-28 15:52:42 +0100

    Add data directory for small data files

---
+++ LSST.py
@@ -0,0 +1,17 @@
+# Procedures for LSST data
+
+from astropy.table import Table, join
+import matplotlib.pyplot as plt
+import numpy as np
+
+def check_mask(datafile, maskfile):
+    '''Plot results of LSST query and GAIA bright star mask.'''
+    t = Table.read(datafile)
+    mask = np.loadtxt(maskfile, skiprows=2)
+
+    ax = plt.sybplot(111)
+    plt.scatter(t['ra'], t['dec'], s=0.1)
+    for i in range(mask.shape[1]):
+        circle = plt.Circle((mask[i, 0], mask[i, 1]), mask[i, 2], color='r', fill=False)
+        ax.add_patch(circle)
+    plt.show()
